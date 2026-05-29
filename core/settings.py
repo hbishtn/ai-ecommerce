1bb94cb498d906a9b279f5c64a4416412b7a3842
himanshu <hbishtn@gmail.com>
2026-05-30 03:07:48 +0530

    fix media error

--- core/settings.py
+++ core/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'orders',
     'accounts',
     'ai_assistant',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +137,11 @@ MEDIA_ROOT = 'media'
 LOGIN_URL = 'login'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
